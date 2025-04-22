bbea02d9c3f3cc6ab620b7e103045cdb66ebc6c1
khang <khangdjnh24042002@gmail.com>
2025-04-22 14:49:25 +0700

    Init Project

---
+++ src/main/java/com/khangdjnh/identity_keycloak/IdentityKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.khangdjnh.identity_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityKeycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khangdjnh/identity_keycloak/IdentityKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khangdjnh.identity_keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
